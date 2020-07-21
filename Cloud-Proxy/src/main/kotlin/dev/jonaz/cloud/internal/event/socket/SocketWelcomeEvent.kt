4a045e94455e70f513d9cfa9f686fd1aaa188cec
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-22 01:51:37 +0200

    [PROXY] Add welcome event

---
+++ Cloud-Proxy/src/main/kotlin/dev/jonaz/cloud/internal/event/socket/SocketWelcomeEvent.kt
@@ -0,0 +1,8 @@
+package dev.jonaz.cloud.internal.event.socket
+
+class SocketWelcomeEvent {
+
+    fun execute() {
+        println("Successfully connected to cloud")
+    }
+}
---
+++ Cloud-Proxy/src/main/kotlin/dev/jonaz/cloud/internal/model/socket/SocketWelcomeModel.kt
@@ -0,0 +1,5 @@
+package dev.jonaz.cloud.internal.model.socket
+
+data class SocketWelcomeModel(
+    val welcome: Boolean
+)
