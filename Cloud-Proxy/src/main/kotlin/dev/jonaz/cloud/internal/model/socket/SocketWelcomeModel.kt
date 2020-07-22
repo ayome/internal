447b68466c3f0e84dcda0dd3bc2bec78feb89e07
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-22 12:58:02 +0200

    [PROXY] Remove SocketWelcomeModel

--- Cloud-Proxy/src/main/kotlin/dev/jonaz/cloud/internal/model/socket/SocketWelcomeModel.kt
+++
@@ -1,5 +0,0 @@
-package dev.jonaz.cloud.internal.model.socket
-
-data class SocketWelcomeModel(
-    val welcome: Boolean
-)
--- Cloud-Proxy/src/main/kotlin/dev/jonaz/cloud/internal/util/proxy/ServerManager.kt
+++ Cloud-Proxy/src/main/kotlin/dev/jonaz/cloud/internal/util/proxy/ServerManager.kt
@@ -7,13 +7,11 @@ class ServerManager {
     private val instance = ProxyServer.getInstance()
 
     fun add(name: String, address: InetSocketAddress, motd: String, restricted: Boolean) {
-        println("test")
         val construct = instance.constructServerInfo(name, address, motd, restricted)
         instance.servers.put(name, construct)
     }
 
     fun remove(name: String) {
-        println("test2")
         ProxyServer.getInstance().servers.remove(name)
     }
 }
